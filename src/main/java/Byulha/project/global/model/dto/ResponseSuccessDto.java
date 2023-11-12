89c7dd2e2eecce73bf6e3f41f6e16e4af5dc65db
gutanbug53 <gutanbug53@gmail.com>
2023-11-13 01:08:19 +0900

    feat: 응답 성공 DTO 생성

---
+++ src/main/java/Byulha/project/global/model/dto/ResponseSuccessDto.java
@@ -0,0 +1,17 @@
+package Byulha.project.global.model.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ResponseSuccessDto {
+
+    private final String message;
+
+    public ResponseSuccessDto() {
+        this("ok");
+    }
+
+    public ResponseSuccessDto(String message) {
+        this.message = message;
+    }
+}
