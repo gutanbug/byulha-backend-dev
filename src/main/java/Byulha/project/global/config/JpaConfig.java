a29ea6d4a2921729c6cc20d49c04d0cd35ff45b4
gutanbug53 <gutanbug53@gmail.com>
2023-11-12 17:10:41 +0900

    feat: Jpa, Security, Web Config 설정 추가

---
+++ src/main/java/Byulha/project/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package Byulha.project.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
