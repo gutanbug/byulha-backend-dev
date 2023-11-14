e7ab157291cc2b4fefd3e00443c782979c4a709d
gutanbug53 <gutanbug53@gmail.com>
2023-11-15 01:59:31 +0900

    fix: 휴대폰 번호 받는 RequestDto 수정

--- src/main/java/Byulha/project/user/model/dto/request/RequestWithPhoneNumberDto.java
+++ src/main/java/Byulha/project/user/model/dto/request/RequestWithPhoneNumberDto.java
@@ -9,11 +9,17 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Getter
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class RequestWithPhoneNumberDto {
 
     @NotBlank
     @Pattern(regexp = "01[0-1|6-9]-?\\d{4}-?\\d{4}")
     @Schema(description = "휴대폰 번호. 대시(-)는 있어도 되고 없어도 된다.", example = "010-1111-2222")
-    private final String phoneNumber;
+    private String phoneNumber;
+
+    public RequestWithPhoneNumberDto() {
+    }
+
+    public RequestWithPhoneNumberDto(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
