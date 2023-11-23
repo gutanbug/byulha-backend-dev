7645f9034f7650ee495080015d4edf77ceafc08a
gutanbug53 <gutanbug53@gmail.com>
2023-11-23 14:52:42 +0900

    feat: 향수 엔티티 클래스 추가

---
+++ src/main/java/Byulha/project/domain/perfume/model/PriceValue.java
@@ -0,0 +1,29 @@
+package Byulha.project.domain.perfume.model;
+
+public enum PriceValue {
+
+    /**
+     * 매우 비쌈
+     */
+    WAY_OVERPRICED,
+
+    /**
+     * 비쌈
+     */
+    OVERPRICED,
+
+    /**
+     * 적당함
+     */
+    OK,
+
+    /**
+     * 저렴함
+     */
+    GOOD_VALUE,
+
+    /**
+     * 매우 저렴함
+     */
+    GREAT_VALUE;
+}
